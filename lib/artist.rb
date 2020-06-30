8052c242abee9887ddbf3904a0f1b8ba9cf4ddf4
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-30 05:05:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,11 @@
 # class Artist
 # song_count = 0
 #   attr_accessor :name, :songs
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def songs
 #     Song.all.select do |song|
 #         song.artist == self
@@ -13,16 +13,16 @@
 # end
 # def add_song(song)
 # song.artist = self
-# 
+#
 # end
-# 
+#
 # def add_song_by_name(name)
 # artist = Song.new(name)
 # add_song(artist)
 # end
-# 
+#
 # def self.song_count
 #   Song.all.count
-# 
+#
 # end
 # end
