5fb6dba5419370bfbebd12d03ef012dfe29034b4
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-30 06:04:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,11 +26,10 @@ end
 #We write this method in this class Artist because it is the artist responsibility to add a new song to their collection
 #
 def add_song_by_name(name)
-#this takes in an argument of a name and genre and both creates the new song and adds that song to the artist's collection.
 artist = Song.new(name)
 add_song(artist)
 end
-#
+#the method above takes in an argument of a name and genre and both creates the new song and adds that song to the artist's collection.
 def self.song_count
   Song.all.count
 
