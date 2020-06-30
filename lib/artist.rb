485a9a0e278a27063a1570df6cd740f1a144b2da
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-30 05:33:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,18 +8,18 @@ class Artist
     @name = name
   end
 #
-  def songs # this method allows us to call on an individual artist to return the list of songs that the artist has 
-    Song.all.select do |song|  
+  def songs # this method allows us to call on an individual artist to return the list of songs that the artist has
+    Song.all.select do |song|
         song.artist == self
   end
 # we use self keyword to refer to the artist on which we are calling this method.
 # We call artist= method on the song that is being passed in as an argument and set it equal to self-the artist
 # telling a song that it belongs to an artist should happen when that song is added to the artist's @songs collection
 end
-def add_song(song) 
+def add_song(song)
 song.artist = self
 end
-#This method will allow us to add new songs. 
+#This method will allow us to add new songs.
 #We write this method in this class Artist because it is the artist responsibility to add a new song to their collection
 #
 # def add_song_by_name(name)
