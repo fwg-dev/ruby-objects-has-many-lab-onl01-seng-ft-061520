c6f6a8ff78acdfaef32f3fbb934707139ee61577
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-30 05:59:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ def initialize(name)
  def self.all# this is a getter method for the class variable
    @@all
 end
-def artist_name #this gives allows us to have one simple and descriptive method that could return the name 
+def artist_name #this gives allows us to have one simple and descriptive method that could return the name
 if artist
   self.artist.name
 else
