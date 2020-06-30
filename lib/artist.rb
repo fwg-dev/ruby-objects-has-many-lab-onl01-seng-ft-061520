273141a8e643cd37d58d4bc8222d98a6f5d9c594
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-30 05:18:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
 class Artist
 # song_count = 0
+# we can set individual instance of Song equal to an instance of Artist class  at the end 
+
 #   attr_accessor :name, :songs
 #
 #   def initialize(name)
@@ -11,10 +13,9 @@ class Artist
 #         song.artist == self
 #   end
 # end
-# def add_song(song)
-# song.artist = self
-#
-# end
+def add_song(song) #This method will allow us to add new songs. We write this method in this class Artist because it is the artist responsibility to add a new song to their collection
+song.artist = self
+end
 #
 # def add_song_by_name(name)
 # artist = Song.new(name)
